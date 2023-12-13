4d2b4f4faf688bb47071c8e3078a110b3ff5f661
JJW-Math <99464519+JJW-Math@users.noreply.github.com>
2023-12-12 18:46:06 -0800

    Create AutoDrive.java

---
+++ src/main/java/frc/robot/commands/autoCommands/AutoDrive.java
@@ -0,0 +1,47 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.autoCommands;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Drivebase;
+
+public class AutoDrive extends CommandBase {
+  private final Drivebase drivebase;
+  private final double waitTime;
+  private double startTime;
+
+  /** Creates a new AutoDrive. */
+  public AutoDrive(Drivebase drivebase, double waitTime) {
+    this.drivebase = drivebase;
+    this.waitTime = waitTime;
+    startTime = Timer.getFPGATimestamp();
+
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(this.drivebase);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    drivebase.robotOrientedDrive(0.5, 0, 0);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    drivebase.robotOrientedDrive(0, 0, 0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return Timer.getFPGATimestamp() - startTime > waitTime;
+  }
+}
