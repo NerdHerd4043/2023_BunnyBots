3bd9db785b7d4f8ccdd6b1cee528902273beb857
Zynh0722 <Zynh0722@gmail.com>
2024-01-23 20:35:35 -0800

    optimized bb

--- src/main/java/cowlib/SwerveModule.java
+++ src/main/java/cowlib/SwerveModule.java
@@ -10,6 +10,7 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Constants.DriveConstants;
@@ -17,70 +18,70 @@ import frc.robot.Constants.DriveConstants.SwervePID;
 
 /** Add your docs here. */
 public class SwerveModule {
-	private CANSparkMax angleMotor;
-	private CANSparkMax speedMotor;
-	private PIDController pidController;
-	private CANCoder encoder;
-	private boolean inverted;
-	private double offset;
+  private CANSparkMax angleMotor;
+  private CANSparkMax speedMotor;
+  private PIDController pidController;
+  private CANCoder encoder;
+  private boolean inverted;
+  private double offset;
   private double maxVoltage;
   private double maxVelocity;
 
-	public SwerveModule(int angleMotor, int speedMotor, int encoder, boolean drive_inverted, double offset, double maxVelocity, double maxVoltage) {
-		this.angleMotor = new CANSparkMax(angleMotor, MotorType.kBrushless);
-		this.speedMotor = new CANSparkMax(speedMotor, MotorType.kBrushless);
-		this.pidController = new PIDController(SwervePID.p, SwervePID.i, SwervePID.d);
-		this.encoder = new CANCoder(encoder);
-		this.inverted = drive_inverted;
-		this.offset = offset;
+  public SwerveModule(int angleMotor, int speedMotor, int encoder, boolean drive_inverted, double offset,
+      double maxVelocity, double maxVoltage) {
+    this.angleMotor = new CANSparkMax(angleMotor, MotorType.kBrushless);
+    this.speedMotor = new CANSparkMax(speedMotor, MotorType.kBrushless);
+    this.pidController = new PIDController(SwervePID.p, SwervePID.i, SwervePID.d);
+    this.encoder = new CANCoder(encoder);
+    this.inverted = drive_inverted;
+    this.offset = offset;
     this.maxVelocity = maxVelocity;
     this.maxVoltage = maxVoltage;
 
-		this.pidController.enableContinuousInput(-180, 180);
-	}
-	
-	public SwerveModule(SwerveModuleConfig config, double maxVelocity, double maxVoltage) {
-		this(config.angleMotorId, config.driveMotorId, config.encoderId, config.drive_inverted, config.offset, maxVelocity, maxVoltage);
-		angleMotor.setSmartCurrentLimit(DriveConstants.currentLimit);
-		speedMotor.setSmartCurrentLimit(DriveConstants.currentLimit);
-	}
+    this.pidController.enableContinuousInput(-180, 180);
+  }
 
-	public void drive(double speedMetersPerSecond, double angle) {
+  public SwerveModule(SwerveModuleConfig config, double maxVelocity, double maxVoltage) {
+    this(config.angleMotorId, config.driveMotorId, config.encoderId, config.drive_inverted, config.offset, maxVelocity,
+        maxVoltage);
+    angleMotor.setSmartCurrentLimit(DriveConstants.currentLimit);
+    speedMotor.setSmartCurrentLimit(DriveConstants.currentLimit);
+  }
+
+  private void drive(double speedMetersPerSecond, double angle) {
     var voltage = (speedMetersPerSecond / maxVelocity) * maxVoltage;
-		speedMotor.setVoltage(voltage * (this.inverted ? -1 : 1));
-		angleMotor.setVoltage(-pidController.calculate(this.getEncoder(), angle));
-		// System.out.println(speed);
-		// angleMotor.setVoltage(-pidController.calculate(clamp(this.getEncoder() + offset), angle));
-	}
+    speedMotor.setVoltage(voltage * (this.inverted ? -1 : 1));
+    angleMotor.setVoltage(-pidController.calculate(this.getEncoder(), angle));
+  }
 
-	public void drive(SwerveModuleState state) {
-		this.drive(state.speedMetersPerSecond, state.angle.getDegrees());
-	}
+  public void drive(SwerveModuleState state) {
+    var optimizedState = SwerveModuleState.optimize(state, new Rotation2d(getEncoderRadians()));
+    this.drive(optimizedState.speedMetersPerSecond, optimizedState.angle.getDegrees());
+  }
 
-	public double getEncoder() {
-		return encoder.getAbsolutePosition();
-	}
+  public double getEncoder() {
+    return encoder.getAbsolutePosition();
+  }
 
-	public double getEncoderRadians() {
-		return encoder.getAbsolutePosition()*2*Math.PI/360.0;
-	}
+  public double getEncoderRadians() {
+    return encoder.getAbsolutePosition() * Math.PI / 180.0;
+  }
 
-	public double getRelativeEncoder() {
-		return clamp(getEncoder() + offset);
-	}
+  public double getRelativeEncoder() {
+    return clamp(getEncoder() + offset);
+  }
 
-	// public void resetEncoder() {
-	// 	offset = -getEncoder();
-	// 	// encoder.configMagnetOffset(offset);
-	// }
+  // public void resetEncoder() {
+  // offset = -getEncoder();
+  // // encoder.configMagnetOffset(offset);
+  // }
 
-	public double clamp(double n) {
-		if(n > 180) {
-			return n - 360;
-		}
-		else if(n < -180) {
-			return 360 + n;
-		}
-		return n;
-	}
+  public double clamp(double n) {
+    if (n > 180) {
+      return n - 360;
+    } else if (n < -180) {
+      return 360 + n;
+    }
+    return n;
+  }
 }
